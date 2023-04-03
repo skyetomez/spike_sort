b6fc2da598c41ebdf4f6c80cd842f644aaa7a819
Skyler Cluster <skylerthomas@umass.edu>
2023-04-03 19:26:23 +0000

    list of TODOs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is the readme for this repo
+# This is the readme for this repo
+
+## TODOS:
+1. Singularity Container
+2. Data Load 
+3. Data Process
+4. Spike Sort
+
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,10 @@
 from extractor import get_ephys_data
 import spikeinterface.extractors as se
 
+""" 
+Is parallel processing enabled by default? 
+"""
+
 # Sources/Rhythm FPGA <- refers to the hardware used for data collection
 
 # ----- preprocessing of signal ---
@@ -17,20 +21,23 @@ def clean_ephys_data(raw_recording):
     # Filters/Channel Map <- get channels
 
     # AVAI: Filters/Common Avg Ref <- Gain applied to selected channels. channels are selected to compute average and this is applied to other channels
-    averaged = se.common_reference(raw_recording, operator="average", reference="global")
+    averaged = se.common_reference(
+        raw_recording, operator="average", reference="global"
+    )
 
     # Utilities/Splitter <- paraprocessing e.g. HPF for spike detect, LPF local field pot.
 
     # AVAI: Filters/Bandpass Filter <- range for filter
-    filtered = se.bandpass_filter(averaged, freq_min = LOWER_CF, freq_max= UPPER_CF )
+    filtered = se.bandpass_filter(averaged, freq_min=LOWER_CF, freq_max=UPPER_CF)
 
     # Utilities/Splitter <- repeat
-    return recording
+    return filtered
 
 
 # Sinks/LFP Viewer <- local field potential viewer
 
 # Filters/Spike Detector <- Detects spikes in continuous data and packages in events
+# Needs to be run in container i.e. Singularity / Docker
 
 # Sinks/Spike Viewer <- local field potential viewer
 
